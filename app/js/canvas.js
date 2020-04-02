f8b9d6d4716f780d9ac916454d44fa3709f95036
Greg Charitonos <2048264+gregcharitonos@users.noreply.github.com>
2020-04-02 18:50:43 +0200

    made basic framework for app

--- app/js/canvas.js
+++ app/js/canvas.js
@@ -1,15 +1,45 @@
 // configures all the canvases
 
-function resetCanvas(canvas) {
-    var config = {
-        fill:"white",
-        stroke:"white"
-    };
-    canvas.width = canvas.offSetWidth;
+class Canvas {
+  config = {
+    fill:"white",
+    stroke:"white"
+  }
+
+  constructor(id,chartData={}, updater=null){
+    var self = this;
+    if(updater != null){//custom animation, not a chart
+      this.updater = updater;
+      this.element = document.createElement("canvas");
+      document.querySelector(`#${id}`).appendChild(this.element);
+      this.ctx = this.element.getContext("2d");
+      this.animationFrame = null;
+
+      this.render = function(updater){
+        var e = self.element;
+        var c = self.ctx;
+        self.animationFrame = requestAnimationFrame(self.render);
+        c.clearRect(0,0,e.width,e.height);
+        self.updater(c,e);
+      }
+      this.resetCanvas();
+    }
+
+    if(Object.entries(chartData).length > 0){//for charts
+      this.chart = new CanvasJS.Chart(id,chartData);
+      this.render = this.chart.render.bind(this.chart);
+      this.element = this.chart.canvas;
+    }
+  }
+
+  resetCanvas(){
+    var canvas = this.element;
+    var ctx = this.ctx;
+    canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-    canvas.ctx = canvas.getContext("2d");
-    canvas.ctx.fill = config.fill;
-    canvas.ctx.stroke = config.stroke;
-    canvas.ctx.clearRect(0,0,canvas.width,canvas.height);
-    return canvas.ctx;
+    ctx.fill = this.config.fill;
+    ctx.stroke = this.config.stroke;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+  }
+
 }
